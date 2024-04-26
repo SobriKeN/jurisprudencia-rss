709880f74182994526e83e9ff0a59e0dd7b361e1
Fábio Sobrinho <fabio.sobrinho@tecnico.ulisboa.pt>
2024-04-26 17:53:03 +0100

    all rss channels done

--- dist/index.js
+++ dist/index.js
@@ -15,13 +15,17 @@ var __asyncValues = (this && this.__asyncValues) || function (o) {
     function verb(n) { i[n] = o[n] && function (v) { return new Promise(function (resolve, reject) { v = o[n](v), settle(resolve, reject, v.done, v.value); }); }; }
     function settle(resolve, reject, d, v) { Promise.resolve(v).then(function(v) { resolve({ value: v, done: d }); }, reject); }
 };
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 const elasticsearch_1 = require("@elastic/elasticsearch");
 const jurisprudencia_document_1 = require("@stjiris/jurisprudencia-document");
 const feed_1 = require("feed");
 const promises_1 = require("fs/promises");
+const path_1 = __importDefault(require("path"));
 const client = new elasticsearch_1.Client({ node: process.env.ES_URL || "http://localhost:9200", auth: { username: "elastic", password: "elasticsearch" } });
-function main() {
+function generateRSSFeedForAll() {
     return __awaiter(this, void 0, void 0, function* () {
         var _a, e_1, _b, _c;
         var _d, _e, _f, _g, _h, _j, _k, _l, _m, _o;
@@ -34,7 +38,6 @@ function main() {
         });
         let p = client.helpers.scrollDocuments({
             index: jurisprudencia_document_1.JurisprudenciaVersion,
-            //_source: ["Número de Processo", "Relator Nome Profissional", "Data"],
             sort: {
                 Data: "desc"
             }
@@ -69,7 +72,7 @@ function main() {
                         "Sumário: " + acordao.Sumário || "Sumário não encontrado",
                     date: data
                 });
-                if (counter >= 5) {
+                if (counter >= 10) {
                     break;
                 }
             }
@@ -81,7 +84,87 @@ function main() {
             }
             finally { if (e_1) throw e_1.error; }
         }
-        yield (0, promises_1.writeFile)("rss.xml", feed.rss2());
+        const aggKey = "rss";
+        const pathToRSS = path_1.default.join(process.env.RSS_FOLDER || "", aggKey + ".xml");
+        yield (0, promises_1.writeFile)(pathToRSS, feed.rss2());
     });
+}
+function generateRSSFeed(inputString) {
+    return __awaiter(this, void 0, void 0, function* () {
+        var _a, e_2, _b, _c;
+        var _d, _e, _f, _g, _h, _j, _k, _l, _m, _o;
+        const feed = new feed_1.Feed({
+            title: 'RSS Jurisprudência - ' + inputString,
+            id: 'http://localhost:3000/jurisprudencia',
+            link: 'http://localhost:3000/jurisprudencia',
+            description: 'Latest updates from Your Website',
+            copyright: 'Supremo Tribunal da Justiça, 2024'
+        });
+        let p = client.helpers.scrollDocuments({
+            index: jurisprudencia_document_1.JurisprudenciaVersion,
+            //_source: ["Número de Processo", "Relator Nome Profissional", "Data"],
+            query: {
+                term: {
+                    "Área.Show": inputString
+                }
+            },
+            sort: {
+                Data: "desc"
+            }
+        });
+        let counter = 0;
+        try {
+            for (var _p = true, p_2 = __asyncValues(p), p_2_1; p_2_1 = yield p_2.next(), _a = p_2_1.done, !_a; _p = true) {
+                _c = p_2_1.value;
+                _p = false;
+                const acordao = _c;
+                counter++;
+                let [dd, mm, yyyy] = ((_d = acordao.Data) === null || _d === void 0 ? void 0 : _d.split("/")) || "01/01/1900".split("/");
+                let data = new Date(parseInt(yyyy), parseInt(mm) - 1, parseInt(dd), 12);
+                let id = ((_e = acordao.ECLI) === null || _e === void 0 ? void 0 : _e.startsWith("ECLI:PT:STJ:")) ? `/ecli/${acordao.ECLI}` : `/${encodeURIComponent(acordao["Número de Processo"])}/${acordao.UUID}`;
+                const descritoresArray = (String)((_f = acordao.Descritores) === null || _f === void 0 ? void 0 : _f.Show).split(",");
+                const descritoresFormatados = descritoresArray.join(" / ");
+                const meioProcessualArray = (String)((_g = acordao["Meio Processual"]) === null || _g === void 0 ? void 0 : _g.Show).split(",");
+                let meioProcessualFormatado;
+                if (meioProcessualArray.length > 1) {
+                    meioProcessualFormatado = meioProcessualArray.join("/");
+                }
+                else {
+                    meioProcessualFormatado = (_h = acordao["Meio Processual"]) === null || _h === void 0 ? void 0 : _h.Show;
+                }
+                feed.addItem({
+                    title: acordao["Número de Processo"] || "Número de Processo não encontrado",
+                    id: id,
+                    link: "localhost:3000" + id,
+                    content: ((_j = acordao.Área) === null || _j === void 0 ? void 0 : _j.Show) + " - " + meioProcessualFormatado + " - " + ((_k = acordao["Relator Nome Profissional"]) === null || _k === void 0 ? void 0 : _k.Show) + " - " + ((_l = acordao.Secção) === null || _l === void 0 ? void 0 : _l.Show) + "<br>" +
+                        "Votação: " + ((_m = acordao.Votação) === null || _m === void 0 ? void 0 : _m.Show) + "&nbsp; &nbsp; &nbsp;" + "Decisão: " + ((_o = acordao.Decisão) === null || _o === void 0 ? void 0 : _o.Show) + "<br>" +
+                        "Descritores: " + descritoresFormatados + "<br> <br>" +
+                        "Sumário: " + acordao.Sumário || "Sumário não encontrado",
+                    date: data
+                });
+                if (counter >= 10) {
+                    break;
+                }
+            }
+        }
+        catch (e_2_1) { e_2 = { error: e_2_1 }; }
+        finally {
+            try {
+                if (!_p && !_a && (_b = p_2.return)) yield _b.call(p_2);
+            }
+            finally { if (e_2) throw e_2.error; }
+        }
+        const aggKey = inputString;
+        const pathToRSS = path_1.default.join(process.env.RSS_FOLDER || "", aggKey + ".xml");
+        yield (0, promises_1.writeFile)(pathToRSS, feed.rss2());
+    });
+}
+function main() {
+    generateRSSFeedForAll();
+    generateRSSFeed("Área Criminal");
+    generateRSSFeed("Área Cível");
+    generateRSSFeed("Área Social");
+    generateRSSFeed("Contencioso");
+    generateRSSFeed("Formação");
 }
 main();
--- src/index.ts
+++ src/index.ts
@@ -2,10 +2,11 @@ import { Client } from "@elastic/elasticsearch";
 import { JurisprudenciaDocument, JurisprudenciaVersion } from "@stjiris/jurisprudencia-document";
 import { Feed } from "feed";
 import { writeFile } from "fs/promises";
+import path from "path";
 
 const client = new Client({ node: process.env.ES_URL || "http://localhost:9200", auth: { username: "elastic", password: "elasticsearch" } })
 
-async function main() {
+async function generateRSSFeedForAll() {
     const feed = new Feed({
         title: 'RSS Jurisprudência',
         id: 'http://localhost:3000/jurisprudencia',
@@ -16,7 +17,6 @@ async function main() {
 
     let p = client.helpers.scrollDocuments<JurisprudenciaDocument>({
         index: JurisprudenciaVersion,
-        //_source: ["Número de Processo", "Relator Nome Profissional", "Data"],
         sort: {
             Data: "desc"
         }
@@ -25,7 +25,7 @@ async function main() {
     let counter = 0
     for await (const acordao of p){
         counter++
-
+        
         let [dd,mm,yyyy] = acordao.Data?.split("/") || "01/01/1900".split("/")
         let data = new Date(parseInt(yyyy),parseInt(mm) - 1,parseInt(dd),12)
         let id = acordao.ECLI?.startsWith("ECLI:PT:STJ:") ? `/ecli/${acordao.ECLI}` : `/${encodeURIComponent(acordao["Número de Processo"]!)}/${acordao.UUID}`
@@ -53,11 +53,87 @@ async function main() {
             date: data 
         });
 
-        if(counter >= 5){
+        if(counter >= 10){
             break;
         }
     }
 
-    await writeFile("rss.xml",feed.rss2())
+    const aggKey = "rss"
+    const pathToRSS = path.join(process.env.RSS_FOLDER || "", aggKey + ".xml")
+    await writeFile(pathToRSS,feed.rss2())
 }
+
+
+async function generateRSSFeed(inputString: string) {
+    const feed = new Feed({
+        title: 'RSS Jurisprudência - ' + inputString,
+        id: 'http://localhost:3000/jurisprudencia',
+        link: 'http://localhost:3000/jurisprudencia',
+        description: 'Latest updates from Your Website',
+        copyright: 'Supremo Tribunal da Justiça, 2024'
+    });
+
+    let p = client.helpers.scrollDocuments<JurisprudenciaDocument>({
+        index: JurisprudenciaVersion,
+        //_source: ["Número de Processo", "Relator Nome Profissional", "Data"],
+        query: {
+            term: {
+                "Área.Show": inputString
+            }
+        },
+        sort: {
+            Data: "desc"
+        }
+    })
+
+    let counter = 0
+    for await (const acordao of p){
+        counter++
+        
+        let [dd,mm,yyyy] = acordao.Data?.split("/") || "01/01/1900".split("/")
+        let data = new Date(parseInt(yyyy),parseInt(mm) - 1,parseInt(dd),12)
+        let id = acordao.ECLI?.startsWith("ECLI:PT:STJ:") ? `/ecli/${acordao.ECLI}` : `/${encodeURIComponent(acordao["Número de Processo"]!)}/${acordao.UUID}`
+        
+        const descritoresArray = (String) (acordao.Descritores?.Show).split(",");
+        const descritoresFormatados = descritoresArray.join(" / ");
+        const meioProcessualArray = (String) (acordao["Meio Processual"]?.Show).split(",");
+        let meioProcessualFormatado;
+        
+        if (meioProcessualArray.length > 1) {
+            meioProcessualFormatado = meioProcessualArray.join("/");
+        } 
+        else {
+            meioProcessualFormatado = acordao["Meio Processual"]?.Show;
+        }
+        
+        feed.addItem({
+            title: acordao["Número de Processo"] || "Número de Processo não encontrado",
+            id: id,
+            link: "localhost:3000" + id,
+            content: acordao.Área?.Show + " - " + meioProcessualFormatado + " - " + acordao["Relator Nome Profissional"]?.Show + " - " + acordao.Secção?.Show + "<br>" +
+                    "Votação: " + acordao.Votação?.Show +  "&nbsp; &nbsp; &nbsp;" + "Decisão: " + acordao.Decisão?.Show + "<br>" +
+                    "Descritores: " + descritoresFormatados + "<br> <br>" + 
+                    "Sumário: " + acordao.Sumário || "Sumário não encontrado",
+            date: data 
+        });
+
+        if(counter >= 10){
+            break;
+        }
+    }
+
+    const aggKey = inputString
+    const pathToRSS = path.join(process.env.RSS_FOLDER || "", aggKey + ".xml")
+    await writeFile(pathToRSS,feed.rss2())
+}
+
+function main(){
+    generateRSSFeedForAll()
+    generateRSSFeed("Área Criminal")
+    generateRSSFeed("Área Cível")
+    generateRSSFeed("Área Social")
+    generateRSSFeed("Contencioso")
+    generateRSSFeed("Formação")
+}
+
 main()
