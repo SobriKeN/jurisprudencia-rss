66fb70a2e04b3c0335e199ca985bcca41e6c7ff9
Fábio Sobrinho <fabio.sobrinho@tecnico.ulisboa.pt>
2024-05-05 15:00:09 +0100

    added max feed size

--- src/index.ts
+++ src/index.ts
@@ -30,7 +30,14 @@ async function main() {
 
     let p = client.helpers.scrollDocuments<JurisprudenciaDocument>({
         index: JurisprudenciaVersion,
-        //_source: ["Número de Processo", "Relator Nome Profissional", "Data"],
+        _source: {
+            excludes: ["CONTENT", "Doutrina", "Fonte", "HASH", "Indicações Eventuais", "Jurisprudência",
+            "Jurisprudência Estrangeira", "Jurisprudência Internacional", "Jurisprudência Nacional",
+            "Legislação Comunitária", "Legislação Estrangeira", "Legislação Nacional", "Original",
+            "Referência de publicação", "Referências Internacionais", "Relator Nome Completo", "STATE",
+            "Texto", "Tipo", "Tribunal de Recurso", "Tribunal de Recurso - Processo", "URL", "Área Temática"
+             ]
+        },
         sort: {
             Data: "desc"
         }
@@ -95,7 +102,7 @@ async function main() {
         });
 
         
-        if(counter >= 1000){
+        if(counter >= parseInt(process.env.RSS_MAX_FEED_SIZE!)){
             break;
         }
     }
